3013c6aa8b0ad08f4394418e0d2517d8fe6dd780
ideenDSAFTU <96814938+ideenDSAFTU@users.noreply.github.com>
2022-04-26 22:53:38 +0700

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,24 @@
+#It is important to use functions when there are many tesks
+#function to add
+#function to subtact
+#function to multiply
+#function to divide
+#Variable declaration
+number1= number2= result =new_num = 0
+operator = ''
+#initiaizing values for variable
+operator = input('select the operator: ')
+
+try:
+    if operator == '+':
+        result = number1 = number2
+        print(' {} + {} ='.format (number1,number2),result)
+    elif operator == '-':
+         result = number1 = number2
+        print(' {} + {} ='.format (number1,number2),result)
+    elif operator == '-':
+         result = number1 = number2
+        print(' {} + {} ='.format (number1,number2),result)
+
+except:
+     print('somthing is ')
